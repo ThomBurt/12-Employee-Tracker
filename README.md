fabfb8a6b8a2f6dce6ea58e83e09790c685ef092
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-02-16 16:59:22 -0500

    figlet installed and working

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Employee Tracker
+A command-line application for managing a company's employees using node, inquirer, and MySQL.
 
 # Dependencies
 - Figlet = create nice title in the console
@@ -6,9 +7,32 @@
 - SQL = mysql for the database
 - console.table
 
-// npm i -y
-// npm init
-// npm install inquirer
-// npm install mysql
-// npm install console.table --save
-// npm i figlet
+npm i -y
+npm install inquirer
+npm install mysql
+npm install console.table --save
+npm i figlet
+
+# USER STORY
+AS A business owner
+I WANT to be able to view and manage the departments, roles, and employees in my company
+SO THAT I can organize and plan my business
+
+# ACCEPTANCE CRITERIA
+GIVEN a command-line application that accepts user input
+WHEN I start the application
+THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+WHEN I choose to view all departments
+THEN I am presented with a formatted table showing department names and department ids
+WHEN I choose to view all roles
+THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+WHEN I choose to view all employees
+THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+WHEN I choose to add a department
+THEN I am prompted to enter the name of the department and that department is added to the database
+WHEN I choose to add a role
+THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+WHEN I choose to add an employee
+THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+WHEN I choose to update an employee role
+THEN I am prompted to select an employee to update and their new role and this information is updated in the database
--- server.js
+++ server.js
@@ -6,18 +6,84 @@ const figlet = require("figlet");
 
 
 // Start server after DB connection
-db.connect((err) => {
+db.connect(function(err) {
+    if (err) throw err;
+    start(); 
+  });
 
-    figlet(' WELCOME TO EMPLOYEE TRACKER', function(err, data) {
-        if (err) {
-            console.log('Something went wrong...');
-            console.dir(err);
-            return;
-        }
-        console.log(data)
-    });
 
-      //STARTS MAIN FUNCTION
- // questions();
-});
 
+function start(){
+    // Figlet opening message
+    console.log('=======================================================================================')
+    console.log(figlet.textSync('Employee Tracker!', {
+        font: 'standard',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 150,
+        whitespaceBreak: true
+    }));
+    console.log('=======================================================================================')
+    console.log('')
+
+
+    // start of inquirer
+    inquirer
+    .prompt ([
+      {
+        type: "list", 
+        message: "What would you like to do?",
+        name: "start",
+        choices: [
+        "Add Employee", 
+        "View all Employees", 
+        "Remove Employee",
+        "Add Department", 
+        "View all Departments",
+        "Add Roles", 
+        "View all Roles", 
+        "Update Employee Role", 
+      ]
+      }
+    ])
+    .then (function(res){
+      switch (res.start){
+  
+        case "Add Employee":
+        addEmployee();
+        break;
+       
+        case "View all Employees":
+        viewAllEmployees();
+        break; 
+  
+        case "Remove Employee": 
+        removeEmployee(); 
+        break;
+      
+        case "Add Department": 
+        addDept(); 
+        break;
+  
+        case "View all Departments":
+        viewAllDept();
+        break;
+  
+        case "Add Roles": 
+        addRole(); 
+        break;
+  
+        case "View all Roles": 
+        viewAllRoles(); 
+        break;
+      
+        case "Update Employee Role":
+        updateEmployeeRole(); 
+        break;
+  
+        case "Exit":
+        connection.end(); 
+        break; 
+      }
+    })
+  }
